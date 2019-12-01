f8f3ec1b8ab22abcf2ee21fb4807c9dd2b0b8f03
tiptop32 <nexus32t@gmail.com>
2019-12-01 15:23:33 +0500

    добавлена оптимизация в виде кэширования списка груп

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -4,8 +4,8 @@ def test_add_group(app):
   old_groups = app.group.get_group_list()
   group = Group(name="test_name", header="awd", footer="awd")
   app.group.create(group)
+  assert app.group.count() - len(old_groups) == 1
   new_groups = app.group.get_group_list()
-  assert len(new_groups) - len(old_groups) == 1
   old_groups.append(group)
   assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
