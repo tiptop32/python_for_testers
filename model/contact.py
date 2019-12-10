10f13dc00bb5f7eca38d70db4ff375f5c1e051ac
tiptop32 <nexus32t@gmail.com>
2019-12-10 21:55:03 +0500

    сравниваем списки всех телефонов объединение и обработка строк

--- model/contact.py
+++ model/contact.py
@@ -2,7 +2,7 @@ from sys import maxsize
 
 class Contact:
 
-    def __init__(self, firstname=None, lastname=None, homephone=None, mobilephone=None, workphone=None, secondaryphone=None, id=None):
+    def __init__(self, firstname=None, lastname=None, homephone=None, mobilephone=None, workphone=None, secondaryphone=None, id=None, all_phones_from_home_page=None):
         self.firstname = firstname
         self.lastname = lastname
         self.homephone = homephone
@@ -10,6 +10,7 @@ class Contact:
         self.workphone = workphone
         self.secondaryphone = secondaryphone
         self.id = id
+        self.all_phones_from_home_page = all_phones_from_home_page
 
     def __repr__(self):
         return "%s:%s %s" % (self.id, self.firstname, self.lastname)
