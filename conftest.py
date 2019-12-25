942056e71bb56a9933eed19894d63c78e5e9641e
tiptop32 <nexus32t@gmail.com>
2019-12-25 13:22:44 +0500

    вынесение параметров в конфигурационный файл

--- conftest.py
+++ conftest.py
@@ -1,19 +1,21 @@
 import pytest
 from fixture.application import Application
+import json
 
 fixture = None
+target = None
 
 @pytest.fixture(scope='session')
 def app(request):
     global fixture
+    global target
     browser = request.config.getoption("--browser")
-    baseUrl = request.config.getoption("--baseUrl")
-    if fixture is None:
-        fixture = Application(browser=browser, baseUrl=baseUrl)
-    else:
-        if not fixture.is_valid():
-            fixture = Application()
-    fixture.session.ensure_login(username="admin", password="secret")
+    if target is None:
+        with open(request.config.getoption("--target")) as config_file:
+            target = json.load(config_file)
+    if fixture is None or not fixture.is_valid():
+        fixture = Application(browser=browser, baseUrl=target['baseUrl'])
+    fixture.session.ensure_login(username=target['username'], password=target['password'])
     return fixture
 
 @pytest.fixture(scope='session', autouse=True)
@@ -26,4 +28,4 @@ def stop(request):
 
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="firefox")
-    parser.addoption("--baseUrl", action="store", default="http://localhost/addressbook/")
+    parser.addoption("--target", action="store", default="target.json")
