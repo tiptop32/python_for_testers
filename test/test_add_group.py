a273124127b2d8b283fea61e275a72a262fb3e0e
tiptop32 <nexus32t@gmail.com>
2019-12-31 13:41:38 +0500

    загрузка тестовых данных из файла

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -2,8 +2,8 @@ from model.group import Group
 import pytest
 from data.groups import testdata
 
-def test_add_group(app, data_groups):
-  group = data_groups
+def test_add_group(app, json_groups):
+  group = json_groups
   old_groups = app.group.get_group_list()
   app.group.create(group)
   assert app.group.count() - len(old_groups) == 1
